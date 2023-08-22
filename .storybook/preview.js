00652ec6ed469de9ae70b77516c3d2a8737ca510
Yuka Naito <yuka.naito@rakus-partners.co.jp>
2023-08-22 18:05:36 +0900

    画面完了

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
 
 import '../src/index.css';
 
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
+
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [mswDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
